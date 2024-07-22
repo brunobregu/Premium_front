fa3640231d368e28236714022c3503b884b12201
Nino S <saulinino@gmail.com>
2024-07-22 18:59:53 +0200

    deleted notification + message icons

--- src/layouts/header-menu-right.tsx
+++ src/layouts/header-menu-right.tsx
@@ -7,8 +7,8 @@ import ChatSolidIcon from '@components/icons/chat-solid';
 
 export default function HeaderMenuRight() {
   return (
-    <div className="ms-auto grid shrink-0 grid-cols-4 items-center gap-2 text-gray-700 xs:gap-3 xl:gap-4">
-      <ActionIcon
+    <div className="ms-auto justify-items-end gap-2 text-gray-700 xs:gap-3 xl:gap-4">
+      {/* <ActionIcon
         aria-label="Notification"
         variant="text"
         className="relative h-[34px] w-[34px] shadow backdrop-blur-md dark:bg-gray-100 md:h-9 md:w-9"
@@ -20,8 +20,8 @@ export default function HeaderMenuRight() {
           enableOutlineRing
           className="absolute right-2.5 top-2.5 -translate-y-1/3 translate-x-1/2"
         />
-      </ActionIcon>
-      <MessagesDropdown>
+      </ActionIcon> */}
+      {/* <MessagesDropdown>
         <ActionIcon
           aria-label="Messages"
           variant="text"
@@ -35,7 +35,7 @@ export default function HeaderMenuRight() {
             className="absolute right-2.5 top-2.5 -translate-y-1/3 translate-x-1/2"
           />
         </ActionIcon>
-      </MessagesDropdown>
+      </MessagesDropdown> */}
 
       <ProfileMenu />
     </div>
